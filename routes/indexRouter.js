752713b968a0498700502ed75693af6d62042c2f
Haileab <womahaileab1428@gmail.com>
2026-04-22 12:10:53 +0300

    started working with routers

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,5 +2,16 @@ const {Router}  = require("express");
 
 const indexRouter = Router();
 
+indexRouter.get("/",  (req, res)=>{
+    console.log("usernames will be logged here - wip");
+    res.send("hello world")
+})
 
+indexRouter.get('/new', (req, res)=>{
+    console.log("username will be formmed")
+})
+
+indexRouter.post('/new', (req, res)=>{
+    console.log("username to be saved; ", req.body.username)
+})
 module.exports = indexRouter;
