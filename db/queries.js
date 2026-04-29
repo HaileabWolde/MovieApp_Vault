9008666e7d14977459fd7779743d9c19365bc439
Haileab <womahaileab1428@gmail.com>
2026-04-29 15:07:07 +0300

    working on getting a specific data detail

--- db/queries.js
+++ db/queries.js
@@ -8,6 +8,9 @@ async function getALLMovies(){
     const {rows} = await pool.query("SELECT * FROM  Movies JOIN Directors ON Movies.director_id = Directors.id");
     return rows;
 }
+async function getSpecficMovies(id){
+    const {rows} = await pool.query(`SELECT * FROM Movies JOIN Directors ON Movies.director_id = Directors.id WHERE movieid = ${id}`)
+}
 async function insertUsername(username){
     await pool.query("INSERT INTO usernames (username) VALUES ($1)", [USERNAME]);
 }
