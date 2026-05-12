3134d006ed2b84d1958745b9c1963b2df7955ba5
Haileab <womahaileab1428@gmail.com>
2026-05-12 15:30:49 +0300

    working with the ui

---
+++ public/script.js
@@ -0,0 +1,43 @@
+
+const myform = document.getElementById('myform');
+const moviename = document.getElementById('moviename');
+const rating = document.getElementById('rating');
+const priority = document.getElementById('priority');
+const dateInput = document.getElementById('watcheddate');
+
+
+const regex = /^[a-zA-Z]+$/;
+//Error
+const movienameError = document.getElementById('movienameError');
+const ratingError = document.getElementById('ratingError');
+
+const validateForm = (event) => {
+    event.preventDefault();
+    // Reset errors
+    movienameError.textContent = '';
+    ratingError.textContent = '';
+
+ 
+
+   // === Movie Name Validation ===
+    if (!moviename.value || moviename.value.trim() === '') {
+        movienameError.textContent = "Movie title is required";
+        movienameError.style.color = '#EA0063';
+       
+    } 
+    else if (!regex.test(moviename.value.trim())) {
+        movienameError.textContent = "Please use only letters, numbers, spaces and common punctuation";
+        movienameError.style.color = '#EA0063';
+      
+    }
+
+    // === Rating Logic (for Already Watched) ===
+    if (priority.value === "0" && (!rating.value || !dateInput.value)) {
+        ratingError.textContent = "Please provide rating and watched date for watched movies";
+        ratingError.style.color = '#EA0063';
+        
+    }
+ 
+};
+
+myform.addEventListener('submit', validateForm);
