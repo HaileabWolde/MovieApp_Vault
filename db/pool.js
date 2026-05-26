da9740207748c5d10984d950f024df23f250f1a6
Haileab <womahaileab1428@gmail.com>
2026-05-26 03:04:44 +0300

    added .env for security reasons

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
-
+require("dotenv").config();   // ← Corrected here
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "postgres",
-  database: "top_users",
-  password: "1428",
-  port: 5432 // The default port
+host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database:  process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT// The default port
 });
