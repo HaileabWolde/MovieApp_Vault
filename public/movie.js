f9197feeaa53c11c4d9c3ace4a5e8b0ebdc00f1c
Haileab <womahaileab1428@gmail.com>
2026-05-26 16:01:01 +0300

    working with delete quieries

---
+++ public/movie.js
@@ -0,0 +1,33 @@
+const mydialogform = document.getElementById('mydialogform')
+//dialgo Showing Logic 
+
+
+const dialog = document.getElementById('dialog')
+const dialogModal = document.getElementById('dialogModal')
+const cancelButton = document.getElementById('cancelButton')
+const deleteMovieButton = document.getElementById('deleteMovieButton'); 
+const movieIdInput = document.getElementById('movieIdInput');
+
+
+deleteMovieButton.addEventListener('click', (e)=> {
+    e.stopPropagation();
+
+    const movieId = deleteMovieButton.dataset.movieId
+    movieIdInput.value = movieId
+      dialogModal.classList.remove('hidden');
+            dialog.showModal(); // Native dialog API
+})
+
+//Cance the modal dialog
+cancelButton.addEventListener('click', ()=> {
+      dialog.close()
+      dialogModal.classList.add('hidden');        // stops form submission
+    
+})
+
+const deleteMovie = (event)=>{
+       event.preventDefault();
+       mydialogform.submit();
+}
+
+mydialogform.addEventListener('submit', deleteMovie);
