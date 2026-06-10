f151470ad0c2b94ae5319e84268e6ba43ac879fd
Haileab <womahaileab1428@gmail.com>
2026-06-10 12:44:57 +0300

    fix db connection for render deployment

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,9 @@
 const { Pool } = require("pg");
-require("dotenv").config();   // ← Corrected here
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
+require("dotenv").config();
+
 module.exports = new Pool({
-host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database:  process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT// The default port
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false   // Required for Render Postgres
+  }
 });
